dbb9dd3ac97b584b66a4e109f57ca0ddde4e2eea
Jacob Coccari <jacob.coccari@gmail.com>
2024-07-18 10:06:09 -0600

    revert

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -5,6 +5,16 @@ import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 const client = await db.connect();
 
 async function seedUsers() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS users (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL
+    );
+  `;
+
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
@@ -20,6 +30,18 @@ async function seedUsers() {
 }
 
 async function seedInvoices() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS invoices (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      customer_id UUID NOT NULL,
+      amount INT NOT NULL,
+      status VARCHAR(255) NOT NULL,
+      date DATE NOT NULL
+    );
+  `;
+
   const insertedInvoices = await Promise.all(
     invoices.map(
       (invoice) => client.sql`
@@ -34,6 +56,17 @@ async function seedInvoices() {
 }
 
 async function seedCustomers() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS customers (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      image_url VARCHAR(255) NOT NULL
+    );
+  `;
+
   const insertedCustomers = await Promise.all(
     customers.map(
       (customer) => client.sql`
@@ -48,6 +81,13 @@ async function seedCustomers() {
 }
 
 async function seedRevenue() {
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS revenue (
+      month VARCHAR(4) NOT NULL UNIQUE,
+      revenue INT NOT NULL
+    );
+  `;
+
   const insertedRevenue = await Promise.all(
     revenue.map(
       (rev) => client.sql`
